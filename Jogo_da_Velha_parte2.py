cca67782c49cb032b8f450131fc74df37fce50d5
BrunaKimura <brunamk@al.insper.edu.br>
2016-04-20 18:25:45 -0300

    Criação da def verifica ganhador

--- Jogo_da_Velha_parte2.py
+++ Jogo_da_Velha_parte2.py
@@ -10,3 +10,24 @@ class Jogo:
             self.jogador == 2
         else:
             self.jogador == 2
+            
+    def verifica_ganhador(self):
+        horizontal1 = self.A[0][0]==self.A[0][1]==self.A[0][2]
+        horizontal2 = self.A[1][0]==self.A[1][1]==self.A[1][2]
+        horizontal3 = self.A[2][0]==self.A[2][1]==self.A[2][2]
+        
+        vertical1 = self.A[0][0]==self.A[1][0]==self.A[2][0]
+        vertical2 = self.A[0][1]==self.A[1][1]==self.A[2][1]
+        vertical3 = self.A[0][2]==self.A[1][2]==self.A[2][2]
+        
+        diagonal1 = self.A[0][0]==self.A[1][1]==self.A[2][2]
+        diagonal2 = self.A[0][2]==self.A[0][1]==self.A[2][0]
+        
+        if horizontal1 or horizontal2 or horizontal3 or vertical1 or vertical2 or vertical3 or diagonal1 or diagonal2 == 1:
+            return 1
+        elif horizontal1 or horizontal2 or horizontal3 or vertical1 or vertical2 or vertical3 or diagonal1 or diagonal2 == 2:
+            return 2
+        elif (horizontal1 or horizontal2 or horizontal3 or vertical1 or vertical2 or vertical3 or diagonal1 or diagonal2) != (1 and 2):
+            return 0 
+        else:
+            return -1
