c919445798abb9d2152feecae4669e85bc765aa3
felipesmn <felipesmn@al.insper.edu.br>
2016-04-23 21:05:03 -0300

    Criei a def verificar e chamei elas nas def clicar e criei a def iniciar

--- Jogo_da_Velha_1.py
+++ Jogo_da_Velha_1.py
@@ -1,11 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 20 19:45:56 2016
-
-@author: felipe
-"""
-
 import tkinter as tk
+from Jogo_da_Velha_parte2 import Jogo
 
 class Tabuleiro:
     
@@ -15,87 +9,118 @@ class Tabuleiro:
         self.window.title("Jogo da Velha!")
         self.window.configure(height=64, width=42)
         
-        botao1 = tk.Button(self.window)
-        botao1.grid(row = 0, column = 0)
-        botao1.configure(command = self.clicar1)
-        botao1.configure(height=7, width=14)
-
-        botao2 = tk.Button(self.window)
-        botao2.grid(row = 0, column = 1)
-        botao2.configure(command = self.clicar2)
-        botao2.configure(height=7, width=14)
-
-        botao3 = tk.Button(self.window)
-        botao3.grid(row = 0, column = 2)
-        botao3.configure(command = self.clicar3)
-        botao3.configure(height=7, width=14)
-
-        botao4 = tk.Button(self.window)
-        botao4.grid(row = 1, column = 0)
-        botao4.configure(command = self.clicar4)
-        botao4.configure(height=7, width=14)
-
-        botao5 = tk.Button(self.window)
-        botao5.grid(row = 1, column = 1)
-        botao5.configure(command = self.clicar5)
-        botao5.configure(height=7, width=14)
-
-        botao6 = tk.Button(self.window)
-        botao6.grid(row = 1, column = 2)
-        botao6.configure(command = self.clicar6)
-        botao6.configure(height=7, width=14)
-
-        botao7 = tk.Button(self.window)
-        botao7.grid(row = 2, column = 0)
-        botao7.configure(command = self.clicar7)
-        botao7.configure(height=7, width=14)
-
-        botao8 = tk.Button(self.window)
-        botao8.grid(row = 2, column = 1)
-        botao8.configure(command = self.clicar8)
-        botao8.configure(height=7, width=14)
-
-        botao9 = tk.Button(self.window)
-        botao9.grid(row = 2, column = 2)
-        botao9.configure(command = self.clicar9)
-        botao9.configure(height=7, width=14)
+        self.meu_jogo = Jogo()
         
-        label = tk.Label(self.window, text = "Próxima jogada: ")
-        label.grid(row = 3)
-        label.configure(height=1)
+        self.botao1 = tk.Button(self.window)
+        self.botao1.grid(row = 0, column = 0)
+        self.botao1.configure(command = self.clicar1)
+        self.botao1.configure(height=7, width=14)
+
+        self.botao2 = tk.Button(self.window)
+        self.botao2.grid(row = 0, column = 1)
+        self.botao2.configure(command = self.clicar2)
+        self.botao2.configure(height=7, width=14)
+
+        self.botao3 = tk.Button(self.window)
+        self.botao3.grid(row = 0, column = 2)
+        self.botao3.configure(command = self.clicar3)
+        self.botao3.configure(height=7, width=14)
+
+        self.botao4 = tk.Button(self.window)
+        self.botao4.grid(row = 1, column = 0)
+        self.botao4.configure(command = self.clicar4)
+        self.botao4.configure(height=7, width=14)
+
+        self.botao5 = tk.Button(self.window)
+        self.botao5.grid(row = 1, column = 1)
+        self.botao5.configure(command = self.clicar5)
+        self.botao5.configure(height=7, width=14)
+
+        self.botao6 = tk.Button(self.window)
+        self.botao6.grid(row = 1, column = 2)
+        self.botao6.configure(command = self.clicar6)
+        self.botao6.configure(height=7, width=14)
+
+        self.botao7 = tk.Button(self.window)
+        self.botao7.grid(row = 2, column = 0)
+        self.botao7.configure(command = self.clicar7)
+        self.botao7.configure(height=7, width=14)
+
+        self.botao8 = tk.Button(self.window)
+        self.botao8.grid(row = 2, column = 1)
+        self.botao8.configure(command = self.clicar8)
+        self.botao8.configure(height=7, width=14)
+
+        self.botao9 = tk.Button(self.window)
+        self.botao9.grid(row = 2, column = 2)
+        self.botao9.configure(command = self.clicar9)
+        self.botao9.configure(height=7, width=14)
+        
+        self.label = tk.Label(self.window, text = "Próxima jogada: X")
+        self.label.grid(row = 3)
+        self.label.configure(height=1)
         
     def clicar1 (self):
-        self.meu_jogo.recebe_jogada(self, 0, 0)
+        self.meu_jogo.recebe_jogada(0, 0)
         self.meu_jogo.jogador
+        self.verifica()
         
     def clicar2 (self):
-        self.meu_jogo.recebe_jogada(self, 0, 1)
+        self.meu_jogo.recebe_jogada(0, 1)
         self.meu_jogo.jogador
+        self.verifica()
         
     def clicar3 (self):
-        self.meu_jogo.recebe_jogada(self, 0, 2)
+        self.meu_jogo.recebe_jogada(0, 2)
         self.meu_jogo.jogador
+        self.verifica()
         
     def clicar4 (self):
-        self.meu_jogo.recebe_jogada(self, 1, 0)
+        self.meu_jogo.recebe_jogada(1, 0)
         self.meu_jogo.jogador
+        if self.meu_jogo.jogador == 1:
+            self.botao4.configure(text="X")
+        else:
+            self.botao4.configure(text="O")
+        self.mudar_Label()
+        self.verifica()
         
     def clicar5 (self):
-        self.meu_jogo.recebe_jogada(self, 1, 1)
+        self.meu_jogo.recebe_jogada(1, 1)
         self.meu_jogo.jogador
+        self.verifica()
         
     def clicar6 (self):
-        self.meu_jogo.recebe_jogada(self, 1, 2)
+        self.meu_jogo.recebe_jogada(1, 2)
         self.meu_jogo.jogador
+        self.verifica()
         
     def clicar7 (self):
-        self.meu_jogo.recebe_jogada(self, 2, 0)
+        self.meu_jogo.recebe_jogada(2, 0)
         self.meu_jogo.jogador
+        self.verifica()
         
     def clicar8 (self):
-        self.meu_jogo.recebe_jogada(self, 2, 1)
+        self.meu_jogo.recebe_jogada(2, 1)
         self.meu_jogo.jogador
+        self.verifica()
         
     def clicar9 (self):
-        self.meu_jogo.recebe_jogada(self, 2, 2)
-        self.meu_jogo.jogador
+        self.meu_jogo.recebe_jogada(2, 2)
+        self.meu_jogo.jogador  
+        self.verifica()
+        
+    def verifica(self):
+        self.meu_jogo.verifica_ganhador()
+        if self.meu_jogo.verifica_ganhador == (1 or 2 or 0):
+            self.meu_jogo.limpa_jogada()
+        else:
+            return -1
+            
+            
+    def iniciar(self):
+        self.window.mainloop() 
+
+jogo = Tabuleiro()
+jogo.iniciar()
+            
