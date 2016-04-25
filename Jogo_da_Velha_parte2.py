4a917e6932bc3e616c9ede6bd934165a2a5ac9e8
felipesmn <felipesmn@al.insper.edu.br>
2016-04-25 20:05:01 -0300

    Ajustes de parenteces e nomeação da lista e do jogador

--- Jogo_da_Velha_parte2.py
+++ Jogo_da_Velha_parte2.py
@@ -2,7 +2,7 @@ class Jogo:
 
 #Criação de um jogador e de uma tabela feita de por uma lista e três sublistas.
     def __init__ (self):
-        self.jogador = 2
+        self.jogador = 1
         self.A = [[0,0,0], [0,0,0], [0,0,0]]
 
 #Esta def rece o local onde foi feita a jogada e o jogador que a fez.        
@@ -75,4 +75,4 @@ class Jogo:
 
    #limpa o tabuleiro de lista e sublista!         
     def limpa_jogada(self):
-        self.tabuleiro = [[0,0,0], [0,0,0], [0,0,0]]
+        self.A = [[0,0,0], [0,0,0], [0,0,0]]
