1b2959adf96e310f01b97a779c90d667bfa1796f
BrunaKimura <brunamk@al.insper.edu.br>
2016-04-25 01:19:56 -0300

    Coloquei comentários no jogo parte 2

--- Jogo_da_Velha_parte2.py
+++ Jogo_da_Velha_parte2.py
@@ -1,9 +1,11 @@
 class Jogo:
 
+#Criação de um jogador e de uma tabela feita de por uma lista e três sublistas.
     def __init__ (self):
         self.jogador = 2
         self.A = [[0,0,0], [0,0,0], [0,0,0]]
-        
+
+#Esta def rece o local onde foi feita a jogada e o jogador que a fez.        
     def recebe_jogada(self, linha, coluna):
         if self.A[linha][coluna] == 0:        
             self.A[linha][coluna]= self.jogador
@@ -14,6 +16,7 @@ class Jogo:
             self.jogador = 1
             
     def verifica_ganhador(self):
+        #todos os modos possíveis de ganhar o jogo.
         horizontal1 = self.A[0][0]==self.A[0][1] and self.A[0][0]==self.A[0][2]
         horizontal2 = self.A[1][0]==self.A[1][1] and self.A[1][0]==self.A[1][2]
         horizontal3 = self.A[2][0]==self.A[2][1] and self.A[2][0]==self.A[2][2]
@@ -25,14 +28,17 @@ class Jogo:
         diagonal1 = self.A[0][0]==self.A[1][1] and self.A[0][0]==self.A[2][2]
         diagonal2 = self.A[0][2]==self.A[0][1] and self.A[0][2]==self.A[2][0]
         
+        #Conferir quem ganhou o jogo.
         if horizontal1 or horizontal2 or horizontal3 or vertical1 or vertical2 or vertical3 or diagonal1 or diagonal2 == 1:
             return 1
         elif horizontal1 or horizontal2 or horizontal3 or vertical1 or vertical2 or vertical3 or diagonal1 or diagonal2 == 2:
             return 2
+        #Neste caso deu velha, ou seja, empate!
         elif (horizontal1 or horizontal2 or horizontal3 or vertical1 or vertical2 or vertical3 or diagonal1 or diagonal2) != (1 and 2):
             return 0 
+        #neste caso o jogo não acabou!
         else:
             return -1
-            
+   #limpa o tabuleiro de lista e sublista!         
     def limpa_jogada(self):
         self.tabuleiro = [[0,0,0], [0,0,0], [0,0,0]]
